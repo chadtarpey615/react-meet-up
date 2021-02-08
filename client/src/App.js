ba0d2c06ff700133e65bc77767ecde18537ac27f
Chad Tarpey <chadtarpey@gmail.com>
2021-02-08 14:38:18 -0600

    created navbar and calendar.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar"
+import Calendar from "./components/Calendar"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
