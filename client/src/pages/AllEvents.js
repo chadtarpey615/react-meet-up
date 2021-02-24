dba7aaa0ac9754d8a895e77b2f1ffb803cd83bbf
Chad Tarpey <chadtarpey@gmail.com>
2021-02-24 16:49:48 -0600

    added userstate into the allevents paga and trying some new styles

--- client/src/pages/AllEvents.js
+++ client/src/pages/AllEvents.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { EventContext } from "../utils/EventStore.js";
+import { UserContext } from "../utils/UserStore";
 import API from "../utils/API";
 import { Container, Col, Row } from "reactstrap";
 
@@ -7,27 +8,35 @@ import { Container, Col, Row } from "reactstrap";
 
 function AllEvents() {
     const { eventState, setEventState } = useContext(EventContext);
+    const { userState, setUserState } = useContext(UserContext);;
     //  console.log(eventState[0].date)
+    console.log(userState)
     useEffect(() => {
         loadEvents();
     }, [])
 
-    const loadEvents = () => {
-        API.findAllEvents()
-            .then(res => {
-                setEventState(res.data)
-                console.log("hit first");
-                console.log(res)
-            })
-            .catch((err) => console.log(err))
+    const loadEvents = async () => {
+
+        try {
+            await API.findAllEvents()
+                .then(res => {
+                    setEventState(res.data)
+                    console.log("hit first");
+                    console.log(res)
+                })
+        }
+        catch (error) {
+            console.log(error)
+        }
+
     };
     return (
         <Container>
-            <h1 className="title"> All events page!!</h1>
+            <h1 className="title"> All events page!! {userState.last_name.toUpperCase()}</h1>
             <Row>
                 {eventState.map(event => (
                     <Col className="col-md-6 col-10">
-                        <div className="all-events">
+                        <div className="all-events" >
                             <h1>Title: {event.name}</h1>
                             <h2>Miles: {event.distance}</h2>
                             <h4>Date: {event.date}</h4>
