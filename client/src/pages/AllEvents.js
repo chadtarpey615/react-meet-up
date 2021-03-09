f58243d7b238509f0fe4cb901d2f0a02a7486957
Chad Tarpey <chadtarpey@gmail.com>
2021-03-08 20:11:00 -0600

    created events.css and added font awesome icons

--- client/src/pages/AllEvents.js
+++ client/src/pages/AllEvents.js
@@ -3,6 +3,8 @@ import { EventContext } from "../utils/EventStore.js";
 import { UserContext } from "../utils/UserStore";
 import API from "../utils/API";
 import { Container, Col, Row } from "reactstrap";
+import { FaRunning } from "react-icons/fa";
+import "../components/events.css";
 
 
 
@@ -33,23 +35,35 @@ function AllEvents() {
     };
     if (eventState.length)
         return (
-            <Container>
+            <>
                 <h1 className="title"> All events page!! {userState.last_name.toUpperCase()}</h1>
-                <Row>
+
+                <div className="section-center section">
+
                     {
-                        eventState.map(event => (
-                            <Col className="col-md-6 col-10">
-                                <div className="all-events" >
-                                    <h1>Title: {event.name}</h1>
-                                    <h2>Miles: {event.distance}</h2>
-                                    <h4>Date: {event.date}</h4>
-                                    <button className="event-btn" onClick={() => alert("gotcha")}>look</button>
-                                </div>
-                            </Col>
-                        ))
+                        eventState.map(event => {
+                            const { id, name, distance, date } = event;
+                            return (
+                                <article className="menu-item" key={id}>
+                                    <FaRunning className="photo" />
+                                    <div className="item-info">
+                                        <header>
+                                            <h4>Title: {name}</h4>
+                                            <h4>Miles: {distance}</h4>
+                                        </header>
+                                        <h4>Date: {date}</h4>
+                                        <button className="event-btn" onClick={() => alert("gotcha")}>look</button>
+                                    </div>
+                                </article>
+                            )
+
+                        })
                     }
-                </Row >
-            </Container >
+                </div>
+            </>
+
+
+
 
         )
     return <h1>Loading events still...</h1>
